7d71bccbdfcaff9f8d94cacf272ebc20eea5faae
Tim Dunn <timdunn@fulcrumgenomics.com>
2024-08-15 19:34:27 -0400

    test: added testthat for all R code

---
+++ tests/testthat/test-bb.test.R
@@ -0,0 +1,80 @@
+test_that("basic bb.test works", {
+    x <- c(1,2,3)
+    tx <- c(4,5,6)
+    group <- c('group1', 'group1', 'group2')
+    expect_equal(bb.test(x, tx, group)[["p.value"]], 0.51927813, tolerance=1e-3)
+})
+
+test_that("bb.test alternative hypotheses work", {
+    x <- c(1,2,3)
+    tx <- c(4,5,6)
+    group <- c('group1', 'group1', 'group2')
+    # default should be two.sided
+    expect_equal(bb.test(x, tx, group), bb.test(x, tx, group, "two.sided"), tolerance=1e-3)
+    # two.sided p-value should be twice 'greater' (since group2 > group1 in this example)
+    expect_equal(bb.test(x, tx, group, "greater")[["p.value"]]*2, 
+                 bb.test(x, tx, group, "two.sided")[["p.value"]], tolerance=1e-3)
+    # less and greater should sum to one
+    expect_equal(bb.test(x, tx, group, "greater")[["p.value"]], 
+                 1-bb.test(x, tx, group, "less")[["p.value"]], tolerance=1e-3)
+})
+
+test_that("bb.test param 'tx' must be positive", {
+    x <- c(1,2,3)
+    tx <- c(0,5,6)
+    group <- c('group1', 'group1', 'group2')
+    expect_error(bb.test(x, tx, group), "tx must be positive.\n")
+})
+
+test_that("bb.test param 'x' must be non-negative", {
+    x <- c(-1,2,3)
+    tx <- c(4,5,6)
+    group <- c('group1', 'group1', 'group2')
+    expect_error(bb.test(x, tx, group), "x must be non-negative.\n")
+})
+
+test_that("bb.test param 'group' must be length ncol(x)", {
+    x <- c(1,2,3)
+    tx <- c(4,5,6)
+    group <- c('group1', 'group2')
+    expect_error(bb.test(x, tx, group), 
+        "length of 'group' must be equal to the number of columns of 'x'")
+})
+
+test_that("bb.test param 'tx' must be length ncol(x)", {
+    x <- c(1,2,3)
+    tx <- c(4,6)
+    group <- c('group1', 'group1', 'group2')
+    expect_error(bb.test(x, tx, group), 
+        "The number of columns of 'tx' must be equal to the number of columns of 'x'")
+})
+
+test_that("bb.test is testing at least two groups", {
+    x <- c(1,2,3)
+    tx <- c(4,5,6)
+    group <- c('group1', 'group1', 'group1')
+    expect_error(bb.test(x, tx, group), "Please compare at least two groups !!!")
+})
+
+test_that("bb.test must test exactly two groups for one-sided tests", {
+    x <- c(1,2,3)
+    tx <- c(4,5,6)
+    group <- c('group1', 'group2', 'group3')
+    expect_error(bb.test(x, tx, group, "less"), "One-sided test for two groups only !!!")
+    expect_error(bb.test(x, tx, group, "greater"), "One-sided test for two groups only !!!")
+})
+
+test_that("bb.test succeeds with empty and full counts", {
+    x <- c(25,34,26,3,14,15)
+    tx <- c(25,34,26,18,32,38)
+    group <- c(rep("na",3),rep("group1",3))
+    expect_equal(bb.test(x,tx,group)[["p.value"]], 5.62228e-05, tolerance=1e-3)
+    expect_equal(bb.test(tx-x,tx,group)[["p.value"]], 5.62228e-05, tolerance=1e-3)
+})
+
+test_that("bb.test succeeds when p = 0.5 exactly", {
+    x <- rep(1,10)
+    tx <- c(7265, 6871, 8452, 8715, 8370, 7955, 7870, 7925, 7885, 8040)
+    group <- c(rep("PRE",5),rep("POST",5))
+    expect_equal(bb.test(x, tx, group, alternative="less")[["p.value"]], 0.5, tolerance=1e-3)
+})
---
+++ tests/testthat/test-fold.change.R
@@ -0,0 +1,40 @@
+test_that("basic fold.change works", {
+    a <- c(1, 2, 3)
+    b <- c(2, 1, 3)
+    c <- c(2, -2, 1)
+    expect_equal(fold.change(a, b), matrix(c, length(c), 1))
+})
+
+test_that("fold.change avoids dividing by zero", {
+    a <- c(0, 2, 3)
+    b <- c(2, 0, 3)
+    c <- c(10000, -10000, 1)
+    expect_equal(fold.change(a, b), matrix(c, length(c), 1))
+})
+
+test_that("fold.change allows overriding BIG", {
+    a <- c(0, 2, 3)
+    b <- c(2, 0, 3)
+    c <- c(100, -100, 1)
+    expect_equal(fold.change(a, b, 100), matrix(c, length(c), 1))
+})
+
+test_that("fold.change works with vectors", {
+    a <- c(1, 2, 3)
+    b <- matrix(c(1, 2, 3, 3, 6, 21), nrow=length(a))
+    c <- matrix(c(2,2,4), nrow=length(a))
+    expect_equal(fold.change(a, b), c)
+})
+
+test_that("fold.change works with scalars", {
+    a <- 2
+    b <- matrix(c(1), nrow=length(a))
+    c <- matrix(c(-2), nrow=length(a))
+    expect_equal(fold.change(a, b), c)
+})
+
+test_that("fold.change fails when the number of rows differ", {
+    a <- c(1, 2, 3)
+    b <- c(1, 2, 3, 4)
+    expect_error(fold.change(a, b))
+})
